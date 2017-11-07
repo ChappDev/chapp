3adb6ef91cc38391441be0bf89479b8f9b77a53f
George <gabolaev98@gmail.com>
2017-11-08 00:27:12 +0300

    First beta version of DB API.

--- redis/includes/Database.h
+++ redis/includes/Database.h
@@ -7,11 +7,42 @@
 
 #include <cpp_redis/cpp_redis>
 
-class Database{
+enum groupType{
+    PUBLIC,
+    PRIVATE,
+    PROTECTED
+};
+
+enum typeOfId{
+    GROUP,
+    USER
+};
+
+class Database {
 
 public:
-    Database();
+
+    Database(std::string ipAddr, int port);
+
+    std::string incrementNowId(typeOfId type);
+
+    int addGroup(groupType type, std::string name);
+    std::map<int, std::string> getListOfGroups(bool showPrivateGroups = 0);
+    void deleteGroup(groupType type, int gid);
+
+    int addUser(std::string username);
+    void deleteUser(int uid);
+
+    void addUserToGroup(int uid, int gid);
+    void deleteUserFromGroup(int uid, int gid);
+    std::vector<int> getUsersInGroup(int gid);
+
 private:
+
+    std::string gidConcat(int gid);
+    std::string ipAddr;
+    int port;
+    cpp_redis::client client;
 
 };
 
