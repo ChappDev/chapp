4a4b3958dd19f80f75b1e7393a1ddccb9dd13d35
George <gabolaev98@gmail.com>
2017-12-09 10:09:21 +0300

    Merged common and redis. Fabric works.

--- test_db.cpp
+++ test_db.cpp
@@ -3,39 +3,40 @@
 //
 
 #include "Database.hpp"
+#include "GroupFactory.hpp"
+#include "UserFactory.hpp"
+#include "User.hpp"
+#include "Group.hpp"
 
 int main(){
 
-    Database &obj = Database::Instance();
+    std::shared_ptr<Chapp::GroupFactory> factory(Chapp::GroupFactory::getInstance());
 
-    auto userId1 = obj.addUser("gabolaev");
-    auto userId2 = obj.addUser("durov");
-    auto userId3 = obj.addUser("buryak");
-    auto userId4 = obj.addUser("vitalya");
-    auto groupId1 = obj.addGroup(Chapp::GroupType::Public, "raggamote", "asldkn");
-    auto result = obj.getGroupInfoById(groupId1);
+    std::shared_ptr<Database> db(Database::getInstance());
 
-    auto groupId11 = obj.addGroup(Chapp::GroupType::Public, "alksmda", "wjaknd");
-    auto groupId2 = obj.addGroup(Chapp::GroupType::Private, "1l2km3", "alksmd");
-    auto groupId22 = obj.addGroup(Chapp::GroupType::Private, "telegram", "1joi23");
-    auto groupId3 = obj.addGroup(Chapp::GroupType::Protected, "buros_developement", "op123k");
-    auto groupId33 = obj.addGroup(Chapp::GroupType::Protected, "l21lkm3123", "ap9skd");
-    std::map<int, std::pair<std::string, Chapp::GroupType>> groups = obj.getListOfGroups();
-    for_each(groups.begin(), groups.end(), [](auto _group){
-        std::cout << _group.first << " = " << _group.second.first << std::endl;
-    });
-    obj.deleteGroup(groupId3);
-//    obj.addUserToGroup(userId1, groupId1);
-//    obj.addUserToGroup(userId2, groupId2);
-    obj.addUserToGroup(userId3, groupId3);
-    obj.addUserToGroup(userId4, groupId3);
-    auto members = obj.getUsersInGroup(groupId3);
-//    obj.deleteUserFromGroup(userId4, groupId3);
-//    obj.deleteUser(userId4);
-//    obj.addUserToGroup(userId1, groupId3);
+    Chapp::chapp_id_t u1index = db->addUser("gabolaev");
+    Chapp::chapp_id_t u2index = db->addUser("tekeev");
+    Chapp::chapp_id_t u3index = db->addUser("movsesov");
+    Chapp::chapp_id_t u4index = db->addUser("ramazanov");
+    Chapp::chapp_id_t u5index = db->addUser("durov");
+    Chapp::chapp_id_t u6index = db->addUser("cat_fil");
+    Chapp::chapp_id_t u7index = db->addUser("big_cat");
 
-//    for_each(members.begin(), members.end(), [](auto member){
-//        std::cout << member << " " << std::endl;
-//
-//    });
+    Chapp::chapp_id_t g1index = db->addGroup(Chapp::GroupType::Private, "raggamote", "sexhashwith20symbols");
+    Chapp::chapp_id_t g2index = db->addGroup(Chapp::GroupType::Private, "telegram", "12345678901234567890");
+
+    db->addUserToGroup(u1index, g1index);
+    db->addUserToGroup(u2index, g1index);
+    db->addUserToGroup(u3index, g1index);
+    db->addUserToGroup(u4index, g1index);
+
+    db->addUserToGroup(u5index, g2index);
+    db->addUserToGroup(u6index, g2index);
+    db->addUserToGroup(u7index, g2index);
+
+    auto group1 = factory->by_id(g1index);
+    auto group2 = factory->by_id(g2index);
+
+    std::cout << "test";
+
 }
